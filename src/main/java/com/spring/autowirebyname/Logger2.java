6252c83ecb5e52014a145b7af8d4305ed1d6f9b2
Raul Zuniga <rzuniga64@gmail.com>
2017-03-09 21:34:33 -0600

    Autowire by type and name.

---
+++ src/main/java/com/spring/autowirebyname/Logger2.java
@@ -0,0 +1,46 @@
+package com.spring.autowirebyname;
+
+/**
+ *  Dummy implementation of Logger2 class.
+ *  We need to wire the ConsoleWriter and FileWriter beans into this class.
+ *  We have two properties of the same type that we want to autowire with beans.
+ */
+public class Logger2 {
+
+    /** Consoler writer. */
+    private LogWriter consoleWriter;
+    /** File writer. */
+    private LogWriter fileWriter;
+
+    /**
+     *  Set the consoleWriter.
+     *  @param writer writer
+     */
+    public void setConsoleWriter(final LogWriter writer) {
+        this.consoleWriter = writer;
+    }
+
+    /**
+     *  Set the fileWriter.
+     *  @param newFileWriter newFileWriter
+     */
+    public void setFileWriter(final LogWriter newFileWriter) {
+        this.fileWriter = newFileWriter;
+    }
+
+    /**
+     *  Write to the file.
+     *  @param text text
+     */
+    public void writeFile(final String text) {
+        fileWriter.write(text);
+    }
+
+    /**
+     *  Write to the console.
+     *  @param text text
+     */
+    public void writeConsole(final String text) {
+        consoleWriter.write(text);
+    }
+}
