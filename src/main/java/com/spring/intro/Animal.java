4c1769ec96e9ed579b68cfb46141829732702eca
Raul Zuniga <rzuniga64@gmail.com>
2017-03-16 01:27:37 -0500

    Update.

--- src/main/java/com/spring/intro/Animal.java
+++ src/main/java/com/spring/intro/Animal.java
@@ -56,7 +56,7 @@ public class Animal {
      *  @param newName name
      *  */
     public void setName(final String newName) {
-        this.name = name;
+        this.name = newName;
     }
 
     /**
