9f60b94c7e629d6b824b51cbbca4dddbe47427af
Raul Zuniga <rzuniga64@gmail.com>
2017-05-08 23:37:10 -0500

    Using logging.

--- src/main/java/com/spring/web/controllers/HomeController.java
+++ src/main/java/com/spring/web/controllers/HomeController.java
@@ -3,18 +3,26 @@ package com.spring.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.apache.log4j.Logger;
+
 /**
  *  HomeController class.
  */
 @Controller
 public class HomeController {
 
+    private static Logger logger = Logger.getLogger(HomeController.class);
     /**
      * Display the home page.
      * @return the home page as a String
      */
     @RequestMapping("/")
     public String showHome() {
+
+        logger.debug("Showing home page...");
+        logger.info("informational message");
+        logger.warn("Warning Message");
+
         return "index";
     }
 }
